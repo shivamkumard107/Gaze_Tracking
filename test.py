ded06896ab70ae42ea26022db1ab5aba9fe26957
Rachit Bansal <rachitbansal2500@hmail.com>
2020-02-17 00:08:41 +0530

    integrated with database

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import cv2
 from gaze_tracking import GazeTracking
 import pyrebase
+import imutils
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 #from gaze_tracking import loss
 
 config = {
@@ -13,6 +17,46 @@ config = {
     "appId": "1:140073311865:web:12426f96cae1f3c88a7ea8",
     "measurementId": "G-H6DWEDR9Z7"
 };
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('firebase-adminsdk.json')
+# Initialize the app with a service account, granting admin privileges
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mcandlefocus.firebaseio.com/'
+})
+
+ref = db.reference('/coordinates')
+
+ref.set({
+    'eye':
+        {
+         'left':{
+                 'x': {},
+                 'y': {}    
+},
+         'right':{
+                 'x': {},
+                 'y': {}
+}
+         },
+    'pupil':
+        {
+             'left':{
+                 'x': {},
+                 'y': {} 
+},
+             'right':{
+                 'x': {},
+                 'y': {} 
+}
+         }
+})
+
+eye_ref_l = ref.child('eye/left')
+pupil_ref_l = ref.child('pupil/left')
+eye_ref_r = ref.child('eye/right')
+pupil_ref_r = ref.child('pupil/right')
 
 firebase = pyrebase.initialize_app(config)
 
@@ -36,11 +80,14 @@ def mean(l):
 
 def helper():
     cap = cv2.VideoCapture("video.mp4")
+    j = 0
     while(cap.isOpened()):
         # We get a new frame from the webcam
         i = 0
         # _, frame = webcam.read()
         ret, frame = cap.read()
+
+        frame = imutils.rotate(frame, 90)
 
         # We send this frame to GazeTracking to analyze it
         gaze.refresh(frame)
@@ -71,10 +118,14 @@ def helper():
             #pupil_loss = tuple(losses.pupil_error(left_pupil), losses.pupil_error(right_pupil, 0))
         	#iris_loss = tuple(losses.iris_error(tuple(x_cords[0], y_cords[0])), losses.iris_error(tuple(x_cords[1], y_cords[1]), 0))
             #print(str(pupil_loss), "\t", str(iris_loss))
+
+            
             print(str(left_pupil), "\t", str(right_pupil), "\t" ,str(x_cords), "\t", str(y_cords))
 
         except:
             continue
+
+        j += 1
 
         """
         try:
@@ -90,9 +141,17 @@ def helper():
             pass
         """
 
+        if(j%10 == 0):
+            eye_ref_l.push({'x':int(left_pupil[0]), 'y':int(left_pupil[1])})
+            eye_ref_r.push({'x':int(right_pupil[0]), 'y':int(right_pupil[1])})
+            pupil_ref_l.push({'x':int(x_cords[0]), 'y':int(x_cords[1])})
+            pupil_ref_r.push({'x':int(y_cords[0]), 'y':int(y_cords[1])})
+
         cv2.imshow("Demo", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        
 
 helper()
