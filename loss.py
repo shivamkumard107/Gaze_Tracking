1641b5dfd1c7990af3ae051d19dab83b08323168
Rachit Bansal <rachitbansal2500@gmail.com>
2020-02-22 01:45:24 +0530

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,28 +5,28 @@ class losses():
 		self.pupil_ideal_left = (310/750, 170/375)
 		self.pupil_ideal_right = (420/750, 170/375)
 		self.iris_ideal_left = (0.5, 0.5)
-		self.iris_ideal_right = (0.5, 0.5)
+		self.iris_ideal_right = (0.55, 0.55)
 
-	def mse(self, x, y):
-		return (x**2 + y**2)**(1/2)
+	def loss(self, x, y, A = 1, B = 1):
+		return A*x + B*y
 
 	def pupil_error(self, pupil_co, left = 1):
 		if left:
-			return self.mse(np.abs(pupil_co[0]/750-self.pupil_ideal_left[0]), 
+			return self.loss(np.abs(pupil_co[0]/750-self.pupil_ideal_left[0]), 
 				np.abs(pupil_co[1]/375-self.pupil_ideal_left[1]))
 
-		return self.mse(np.abs(pupil_co[0]/750-self.pupil_ideal_right[0]), 
+		return self.mse(np.loss(pupil_co[0]/750-self.pupil_ideal_right[0]), 
 				np.abs(pupil_co[1]/375-self.pupil_ideal_right[1]))
 
-	def iris_error(self, iris_co, x = 1):
+	def iris_error(self, iris_co, left = 1):
 		if left:
-			return self.mse(np.abs(iris_co[0]-self.iris_ideal_left[0]),
-				np.abs(iris_co[1]-self.iris_ideal_left[1]))
+			return self.loss(np.abs(iris_co[0]-self.iris_ideal_left[0]),
+				np.abs(iris_co[1]-self.iris_ideal_left[1]), A = 0.7, B = 0.3)
 
-		return self.mse(np.abs(iris_co[0]-self.iris_ideal_right[0]),
-				np.abs(iris_co[1]-self.iris_ideal_right[1]))
+		return self.loss(np.abs(iris_co[0]-self.iris_ideal_right[0]),
+				np.abs(iris_co[1]-self.iris_ideal_right[1]), A = 0.7, B = 0.3)
 
-	def net_loss(self, pupil_co, iris_co, A = 0.5, B = 0.5, left = 1):
+	def net_loss(self, pupil_co, iris_co, A = 1, B = 1, left = 1):
 		L1 = self.pupil_error(pupil_co)
 		L2 = self.iris_error(iris_co)
 		return A*L1 + B*L2
